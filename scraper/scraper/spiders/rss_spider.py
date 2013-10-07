916330d38206e5a9ee05173acf98234f27af5805
username <you@example.com>
2013-10-07 01:03:44 -0700

    metadata implemented for soundcloud and youtube, outputting to console

--- scraper/scraper/spiders/rss_spider.py
+++ scraper/scraper/spiders/rss_spider.py
@@ -16,41 +16,42 @@ class RSSSpider(BaseSpider):
         ]
 
     urls_replace_dict_soundcloud = {'https://w.soundcloud.com/player/?url=': '',
-                         'http://w.soundcloud.com/player/?url=': '',
-                         '%3A': ':',
-                         '%2F': '/',
-                         '?feature=player': '',
-                         '?secret': '',
-                         '%3Fsecret': '',
-                         '?wmode=transparent': '',
-                         '/download': '',
-                         'http://w.soundcloud.com/player?url=': '',
-                         '/embed/': '/v/',
-                         'watch?v=': 'v/',
-                         '?feature=watch': '',
-                         '_embedded': '',
-                         '_detailpage': '',
-                         }
+                                    'http://w.soundcloud.com/player/?url=': '',
+                                    '%3A': ':',
+                                    '%2F': '/',
+                                    '?feature=player': '',
+                                    '?secret': '',
+                                    '%3Fsecret': '',
+                                    '?wmode=transparent': '',
+                                    '/download': '',
+                                    'http://w.soundcloud.com/player?url=': '',
+                                    '/embed/': '/v/',
+                                    'watch?v=': 'v/',
+                                    '?feature=watch': '',
+                                    '_embedded': '',
+                                    '_detailpage': '',
+                                    }
 
     urls_replace_dict_youtube = {'%3A': ':',
-                         '%2F': '/',
-                         '?feature=player': '',
-                         '?secret': '',
-                         '%3Fsecret': '',
-                         '?wmode=transparent': '',
-                         '/download': '',
-                         '/embed/': '/v/',
-                         'watch?v=': 'v/',
-                         '?feature=watch': '',
-                         '_embedded': '',
-                         '_detailpage': '',
-                         }
+                                 '%2F': '/',
+                                 '?feature=player': '',
+                                 '?secret': '',
+                                 '%3Fsecret': '',
+                                 '?wmode=transparent': '',
+                                 '/download': '',
+                                 '/embed/': '/v/',
+                                 'watch?v=': 'v/',
+                                 '?feature=watch': '',
+                                 '_embedded': '',
+                                 '_detailpage': '',
+                                 }
+
 
     def parse(self, response):
         content = response.body
         requests = []
 
-        # soundcloud
+        # soundcloud - find and clean urls, add resolve requests
         urls_soundcloud = re.findall(r'((https?\:)?//(w\.)?soundcloud\.com/[a-zA-Z0-9\+&@#\-\_/=\?%\.]+)', content)
         urls_soundcloud = [i[0] for i in urls_soundcloud]
         for i, j in self.urls_replace_dict_soundcloud.iteritems():
@@ -58,11 +59,10 @@ class RSSSpider(BaseSpider):
         urls_soundcloud = [i.split('?')[0] for i in urls_soundcloud]
         urls_soundcloud = [i.split('&')[0] for i in urls_soundcloud]
         urls_soundcloud = [i.split('_')[0] for i in urls_soundcloud]
-        #print urls_soundcloud
         for url in urls_soundcloud:
-            requests.append(Request(url='http://api.soundcloud.com/resolve.json?url='+url+'&client_id=7d1117f42417d715b28ef9d59af7d57c&format=json&_status_code_map[302]=200', callback=self.parse_metadata_soundcloud, meta={'url': url}))
+            requests.append(Request(url='http://api.soundcloud.com/resolve.json?url='+url+'&client_id=7d1117f42417d715b28ef9d59af7d57c&format=json&_status_code_map[302]=200', callback=self.parse_resolve_soundcloud))
 
-        # youtube
+        # youtube - find urls and ids, add metadata requests
         urls_youtube = re.findall(r'((https?\:)?//www\.youtube\.com/[a-zA-Z0-9\+&@#\-\_/=\?%\.]+)', content)
         urls_youtube = [i[0] for i in urls_youtube]
         for i, j in self.urls_replace_dict_youtube.iteritems():
@@ -72,17 +72,32 @@ class RSSSpider(BaseSpider):
         urls_youtube = [i.split('_')[0] for i in urls_youtube]
         for i in range(len(urls_youtube)):
             if urls_youtube[i].find('youtube') >= 0:
-                url = urls_youtube[i].split('outube.com/v/')
-                url = url[1] if len(url) > 1 else ''
-                url = 'http://www.youtube.com/v/'+url[0:12]
-                urls_youtube[i] = url
-        print urls_youtube
+                id = urls_youtube[i].split('outube.com/v/')
+                id = id[1] if len(id) > 1 else ''
+                id = id[0:12]
+                requests.append(Request(url='http://gdata.youtube.com/feeds/api/videos/'+id+'?v=2&alt=jsonc', callback=self.parse_metadata_youtube))
 
-        # return metadata requests
+        # return requests
         return requests
+
+
+    def parse_resolve_soundcloud(self, response):
+        content = response.body
+        content_dict = json.loads(content)
+        requests = []
+        if content_dict['status'] == '302 - Found':
+            requests.append(Request(url=content_dict['location'], callback=self.parse_metadata_soundcloud))
+        return requests
+
 
     def parse_metadata_soundcloud(self, response):
         content = response.body
-        print json.loads(content)
-        #print response.meta['url']
-        
+        content_dict = json.loads(content)
+        print content_dict
+
+    def parse_metadata_youtube(self, response):
+        content = response.body
+        content_dict = json.loads(content)
+        print content_dict
+
+
